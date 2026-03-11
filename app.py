48f5e98be4cb0fa6b0d7dcd212a6ff2d6fd93988
Peter kanyori <peterynaenah23@gmail.com>
2026-03-11 21:02:44 +0300

    called and ran the flask app

--- app.py
+++ app.py
@@ -12,9 +12,14 @@ bcrypt = Bcrypt()
 def create_app():
      app = Flask(__name__) 
      app.config.from_object(Config) 
-     db.init_app(app) migrate.init_app(app, db) 
+     db.init_app(app) 
+     migrate.init_app(app, db) 
      bcrypt.init_app(app) 
      CORS(app)
      from routes import register_routes 
      register_routes(app)
      return app
+
+app = create_app()
+if __name__ == "__main__": 
+     app.run(debug=True)
