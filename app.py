18f00a53205ff7b80ea9b63df8a756e97d5bd1a2
Peter kanyori <peterynaenah23@gmail.com>
2026-03-11 20:46:55 +0300

    created instances of the flask libraries

--- app.py
+++ app.py
@@ -4,3 +4,7 @@ from flask_migrate import Migrate
 from flask_bcrypt import Bcrypt 
 from flask_cors import CORS
 from config import Config
+
+db = SQLAlchemy()
+migrate = Migrate()
+bcrypt = Bcrypt()
