f8474eb3d29500935327c11073f5ff4ae0adc292
Mark <mark.irungu@student.moringaschool.com>
2026-03-12 17:42:50 +0300

    Renamed file correctly and removed typo

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask_sqlalchemyimport SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_bcrypt import Bcrypt 
 from flask_cors import CORS
