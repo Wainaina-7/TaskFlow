580fe1cf9e7b63eef777ebcfab0061e9d3cc3c36
markirungu <mark.irungu@student.moringaschool.com>
2026-03-15 18:58:33 +0300

    react file static path debug

--- server/app.py
+++ server/app.py
@@ -9,22 +9,23 @@ app = create_app()
 from routes import register_routes
 register_routes(app)
 
-# Absolute path to React build folder - confirmed from debug endpoint
+# The confirmed path to the React build folder on Render
 REACT_BUILD_DIR = '/opt/render/project/src/client/build'
 
 # Serve static files from the React build folder
 @app.route('/<path:path>')
 def serve_static_files(path):
-    """Serve any file from the React build directory if it exists."""
-    full_path = os.path.join(REACT_BUILD_DIR, path)
-    if os.path.exists(full_path) and os.path.isfile(full_path):
+    """Serve any file from the React build directory."""
+    # Try to serve the requested file directly
+    try:
         return send_from_directory(REACT_BUILD_DIR, path)
-    # If file doesn't exist, let the main route handle it
-    return serve_react('')
+    except:
+        # If the file isn't found, let the main route handle it
+        return serve_react('')
 
 @app.route('/')
 def serve_react(path=''):
-    """Serve the main React app."""
+    """Serve the main React app's index.html for all other routes."""
     return send_from_directory(REACT_BUILD_DIR, 'index.html')
 
 if __name__ == "__main__":
