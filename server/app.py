8bccf5eeaac9efe3564439291f33b4e22e87ea7b
Mark <mark.irungu@student.moringaschool.com>
2026-03-16 01:13:57 +0300

    test AGAIN

--- server/app.py
+++ server/app.py
@@ -5,25 +5,19 @@ from flask import send_from_directory
 
 app = create_app()
 
-# Import routes after app is created
 from routes import register_routes
 register_routes(app)
 
-# Get the absolute path to the React build folder
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 REACT_BUILD_DIR = os.path.abspath(os.path.join(BASE_DIR, '..', 'client', 'build'))
 
 print(f"Serving React from: {REACT_BUILD_DIR}", flush=True)
 
-# Serve static files (CSS, JS, images)
-@app.route('/static/<path:filename>')
-def serve_static(filename):
-    return send_from_directory(os.path.join(REACT_BUILD_DIR, 'static'), filename)
-
-# Serve the main React app for all other routes
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 def serve_react(path):
+    if path.startswith('static/'):
+        return send_from_directory(REACT_BUILD_DIR, path)
     return send_from_directory(REACT_BUILD_DIR, 'index.html')
 
 if __name__ == "__main__":
