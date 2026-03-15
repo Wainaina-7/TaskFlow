50b3c0dd2e4e97ffc44f68ee9544438c839d62fe
Mark <mark.irungu@student.moringaschool.com>
2026-03-15 17:42:54 +0300

    add route to serve React frontend

--- server/app.py
+++ server/app.py
@@ -7,5 +7,17 @@ app = create_app()
 from routes import register_routes
 register_routes(app)
 
+# minor change to test deployment
+import os
+from flask import send_from_directory
+
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def serve_react(path):
+    """Serve React static files"""
+    if path and os.path.exists(os.path.join('../client/build', path)):
+        return send_from_directory('../client/build', path)
+    return send_from_directory('../client/build', 'index.html')
+
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
